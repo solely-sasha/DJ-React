a5e90d4053868b91acd2cd15a1a45d9fa0123680
Sasha Sims <solely.sashasims@gmail.com>
2023-03-24 17:47:19 -0400

    finished

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Squares from "./components/Squares";
 
 function App() {
   return (
     <div className="App">
       <h1> DJ React</h1>
+      <Squares />
     </div>
   );
 }
